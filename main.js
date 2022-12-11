4f01b47abe0fe500429b6859590bbf87cc0097c9
Karlsson0214 <karlsson0214@gmail.com>
2022-12-11 19:56:10 +0100

    first

---
+++ main.js
@@ -0,0 +1,11 @@
+// Write a message to the console.
+console.log('hello world!');
+console.log("Hello again!");
+
+console.log("The type of 42 is " + typeof 42); // number
+console.log("The type of 3.14 is " + typeof 3.14); // number
+console.log("The type of '42' is " + typeof '42'); // string
+console.log('The type of "42" is ' + typeof "42"); // string
+console.log("The type of true is " + typeof true); // boolean
+console.log("The type of false is " + typeof false); // boolean
+console.log("The type of null is " + typeof null); // object
